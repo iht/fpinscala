dea2fc41503c611f7f3dae79f4892bbfbe6a54e2
Israel Herraiz <israel.herraiz@gmail.com>
2014-09-29 23:56:50 +0200

    Check if sorted, tail recursive, with test. Exercise 2.2

---
+++ src/main/scala/chap2/SortedApp.scala
@@ -0,0 +1,54 @@
+/* Copyright (c) 2014 Israel Herraiz <isra@herraiz.org>
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */  
+
+package chap2
+
+object SortedApp extends App {
+  // Check if input is sorted from greater to minor
+  val input = args.map(_.toInt)
+
+  // Use to check order from greater to minor
+  val sortFunc = (a: Int, b: Int) => a>=b
+
+  // Use to check order from minor to greater
+  // val sortFunc = (a: Int, b: Int) => a<=b
+
+  val output = isSorted(input, (a:Int, b:Int) => a >= b) match {
+    case true =>
+      "is sorted from greater to minor"
+    case false =>
+      "is not sorted from greater to minor"
+  }
+  
+  println("The list %s %s".format(input.mkString(","), output))
+
+  def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int, s: Boolean): Boolean = {
+      if (n >= as.length - 1) s
+      else loop(n + 1, s && ordered(as(n), as(n+1)))
+    }
+
+    loop(0, true)
+  }
+}
