fe20eb87e6dcc47571602fd69c8ae12fe336125d
Israel Herraiz <israel.herraiz@gmail.com>
2014-09-29 23:19:26 +0200

    Add license header to source

--- src/main/scala/chap2/fibo.scala
+++ src/main/scala/chap2/fibo.scala
@@ -1,3 +1,26 @@
+/* Copyright (c) 2014 Israel Herraiz <isra@herraiz.org>
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */  
+
 package chap2
 
 object FibonacciApp extends App {
@@ -6,14 +29,12 @@ object FibonacciApp extends App {
   println("The Fibonacci number at %d is %d".format(x, fibo(x)))
 
   def fibo(n: Int): Int = {
-
     @annotation.tailrec
     def go(a: Int, b: Int, f: Int): Int = {
       if (f == 0) b
       else if(f == 1) b
       else go(b, a+b, f-1)
     }
-
     go(0, 1, n+1)
   }
 }
