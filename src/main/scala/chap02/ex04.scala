abf68225326211bf97f98549a06df8db4bdc7233
Israel Herraiz <israel.herraiz@gmail.com>
2014-09-30 21:53:02 +0200

    Example 2.4

---
+++ src/main/scala/chap02/ex04.scala
@@ -0,0 +1,36 @@
+/* Copyright (c) 2014 Israel Herraiz <isra@herraiz.org>
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */  
+
+// --------------------
+// Code for example 2.4
+// --------------------
+
+package chap02
+
+object Ex04 {
+
+  def uncurry[A,B,C](f: A => B => C): (A,B) => C = {
+    (x: A, y: B) => f(x)(y)
+  }
+
+}
