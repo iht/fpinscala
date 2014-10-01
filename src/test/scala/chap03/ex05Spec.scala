ba1f5045fb5d5f57ef0f81070a1e846deca132c0
Israel Herraiz <israel.herraiz@gmail.com>
2014-10-02 00:01:12 +0200

    Exercise 3.5

---
+++ src/test/scala/chap03/ex05Spec.scala
@@ -0,0 +1,44 @@
+/* Copyright (c) 2014 Israel Herraiz <isra@herraiz.org>
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */  
+
+// --------------------
+// Test for example 3.5
+// --------------------
+
+package chap03
+
+import org.specs2.mutable._
+import chap03.Ex05.dropWhile
+
+object Ex05Spec extends Specification {
+  "The dropWhile function" should {
+    "remove the first three even integers" in {
+      dropWhile(List(4,2,6,7,5), (x: Int) => x%2 == 0) mustEqual List(7,5)
+    }
+
+    "create Nil" in {
+      dropWhile(List("a","b","c"), (x: String) => x.length < 2) mustEqual Nil
+    }
+  }
+
+}
