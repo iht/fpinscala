da65bdba735375b9799d51669d854bf5359ce096
Israel Herraiz <israel.herraiz@gmail.com>
2014-10-16 22:25:57 +0200

    Example 3.17, naive version

---
+++ src/test/scala/chap03/ex17Spec.scala
@@ -0,0 +1,46 @@
+/* Copyright (c) 2014 Israel Herraiz <isra@herraiz.org>
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */  
+
+// ---------------------
+// Test for example 3.17
+// ---------------------
+
+package chap03
+
+import org.specs2.mutable._
+
+object Ex17Spec extends Specification {
+  "The double2String function" should {
+    "return Nil with Nil" in {
+      Ex17.double2String(Nil) mustEqual Nil
+    }
+     
+    "return a single char with a single value" in {
+      Ex17.double2String(List(1.0)) mustEqual List("1.0")
+    }
+
+    "return a list of strings" in {
+      Ex17.double2String(List(4.0,6.0,5.0,7.2)) mustEqual List("4.0", "6.0", "5.0", "7.2")
+    }    
+  }
+}
