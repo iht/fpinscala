2e81a4bded655cc3b6b20e32f9ce1f8ed24804ca
Israel Herraiz <israel.herraiz@gmail.com>
2014-10-18 14:51:13 +0200

    Exercise 3.22, naive version

---
+++ src/main/scala/chap03/ex22.scala
@@ -0,0 +1,45 @@
+/* Copyright (c) 2014 Israel Herraiz <isra@herraiz.org>
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */  
+
+// ---------------------
+// Code for example 3.22
+// ---------------------
+
+package chap03
+
+import scala.math.Numeric
+
+object Ex22 {
+  
+  def addTwoLists[A: Numeric](l1: List[A], l2: List[A]): List[A] = {
+    assert(l1.length == l2.length)
+
+    val add = implicitly[Numeric[A]].plus _
+
+    (l1, l2) match {
+      case (Nil, _) => Nil
+      case (_, Nil) => Nil
+      case (h1::t1, h2::t2) => add(h1,h2) :: addTwoLists(t1,t2)
+    }
+  }
+}
---
+++ src/test/scala/chap03/ex22Spec.scala
@@ -0,0 +1,50 @@
+/* Copyright (c) 2014 Israel Herraiz <isra@herraiz.org>
+ * 
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+ * BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */  
+
+// ---------------------
+// Test for example 3.22
+// ---------------------
+
+package chap03
+
+import org.specs2.mutable._
+
+object Ex22Spec extends Specification {
+  "The addTwoLists function" should {
+    "return Nil with Nils" in {
+      Ex22.addTwoLists(Nil: List[Double], Nil: List[Double]) mustEqual Nil
+    }
+
+    "fail with Lists of different size" in {
+     Ex22.addTwoLists(List(1,2,3), List(4,5)) must throwA[java.lang.AssertionError]
+    }
+
+    "work with lists of integers" in {
+      Ex22.addTwoLists(List(1,2,3), List(4,5,6)) mustEqual List(5,7,9)
+    }
+
+    "work with lists of doubles" in {
+      Ex22.addTwoLists(List(1.,2.,3.), List(4.,5.,6.)) mustEqual List(5.,7.,9.)
+    }
+  }
+}
