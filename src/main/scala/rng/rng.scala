e505c56e63a796ff7c3be924e556851b7516d226
israel.herraiz <israel.herraiz@amadeus.com>
2015-03-22 10:07:34 +0100

    Exercise 6.10, adding methods to case class, using State class

--- src/main/scala/rng/rng.scala
+++ src/main/scala/rng/rng.scala
@@ -197,58 +197,63 @@ object RNG {
 }
 
 // Exercise 6.10
-case class State[S,+A](run: S => (A,S))
+case class State[S,+A](run: S => (A,S)) {
 
-object State {
-
-  def map[S,A,B](a: S => (A,S))(f: A => B): S => (B,S) = {
-    { s =>
-      val (va,s1) = a(s)
+  def map[B](f: A => B): State[S,B] = {
+    State { s =>
+      val (va,s1) = run(s)
       (f(va), s1)
     }
   }
 
-  def unit[S,A](a: A): S => (A,S) = {
-    { s => (a,s) }
-  }
-
-  def flatMap[S,A,B](f: S => (A,S))(g: A => S => (B,S)): S => (B,S) = {
-    s => {
-      val (a, s2) = f(s)
-      val (b, s3) = g(a)(s2)
-      (b, s3)
+  def flatMap[B](g: A => State[S,B]): State[S,B] = {
+    State {
+      s => {
+        val (a, s2) = run(s)
+        val (b, s3) = g(a).run(s2)
+        (b, s3)
+      }
     }
   }
 
-  def map2[S,A,B,C](a: S => (A,S), b: S => (B,S))(f: (A,B) => C): S => (C,S) = {
-    val sab = { s: S =>
-      val (va,s1) = a(s)
-      val (vb,s2) = b(s1)
+  def map2[B,C](b: State[S,B])(f: (A,B) => C): State[S,C] = {
+    val sab = State { s: S =>
+      val (va,s1) = run(s)
+      val (vb,s2) = b.run(s1)
 
       ((va,vb),s2)
     }
 
-    flatMap(sab) {
+    sab.flatMap {
       case (a,b) =>
-        { s3 => (f(a,b), s3)}
+        State { s3 => (f(a,b), s3)}
     }
   }
+}
 
-  def sequence[S,A](fs: List[S => (A,S)]): S => (List[A],S) = {
-    s => {
+object State {
 
-      @annotation.tailrec
-      def loop(tail: List[S => (A,S)], build: (List[A], S)): (List[A], S) = {
-        tail match {
-          case Nil => build
-          case h :: t =>
-            val (l, s2) = build
-            val (a, s3) = h(s)
-            loop(t, (l :+ a, s3))
+  def unit[S,A](a: A): State[S,A] = {
+    State{ s => (a,s) }
+  }
+
+  def sequence[S,A](fs: List[State[S,A]]): State[S, List[A]] = {
+    State {
+      s => {
+
+        @annotation.tailrec
+        def loop(tail: List[State[S,A]], build: (List[A], S)): (List[A], S) = {
+          tail match {
+            case Nil => build
+            case h :: t =>
+              val (l, s2) = build
+              val (a, s3) = h.run(s)
+              loop(t, (l :+ a, s3))
+          }
         }
-      }
 
-      loop(fs, (Nil: List[A], s))
+        loop(fs, (Nil: List[A], s))
+      }
     }
   }
 }
--- src/test/scala/chap06/ex10Spec.scala
+++ src/test/scala/chap06/ex10Spec.scala
@@ -40,7 +40,7 @@ object Ex10Spec extends Specification {
       def f = { x: Double => x*x}
 
       val left = RNG.map(d)(f)(rng)
-      val right = State.map(d)(f)(rng)
+      val right = State(d).map(f).run(rng)
 
       left mustEqual right
     }
@@ -50,7 +50,7 @@ object Ex10Spec extends Specification {
       def f = { x: Int => x - 4}
 
       val left = RNG.map(d)(f)(rng)
-      val right = State.map(d)(f)(rng)
+      val right = State(d).map(f).run(rng)
 
       left mustEqual right
     }
@@ -59,19 +59,24 @@ object Ex10Spec extends Specification {
   "The State unit method" should {
     "return the same results as RNG unit" in {
 
-      RNG.unit(4)(rng) mustEqual State.unit(4)(rng)
-      RNG.unit(3.14159)(rng) mustEqual State.unit(3.14159)(rng)
-      RNG.unit("Hello world")(rng) mustEqual State.unit("Hello world")(rng)
+      RNG.unit(4)(rng) mustEqual State.unit(4).run(rng)
+      RNG.unit(3.14159)(rng) mustEqual State.unit(3.14159).run(rng)
+      RNG.unit("Hello world")(rng) mustEqual State.unit("Hello world").run(rng)
     }
   }
+
   "The State map2 method" should {
     "calculate the sum of two doubles as RNG map2 does" in {
       val d1 = RNG.doubleWithMap(rng)
       val d2 = RNG.doubleWithMap(d1(rng)._2)
 
       def f = { (a: Double, b: Double) => a + b }
-      
-      val left = State.map2(d1, d2)(f)(rng)
+
+      val sd1 = State(d1)
+      val sd2 = State(d2)
+      val mp  = sd1.map2(sd2)(f)
+
+      val left = mp.run(rng)
       val right = RNG.map2(d1, d2)(f)(rng)
 
       left mustEqual right
@@ -82,8 +87,12 @@ object Ex10Spec extends Specification {
       val d2 = RNG.doubleWithMap(d1(rng)._2)
 
       def f = { (a: Double, b: Double) => a * b }
-      
-      val left = State.map2(d1, d2)(f)(rng)
+
+      val sd1 = State(d1)
+      val sd2 = State(d2)
+      val mp  = sd1.map2(sd2)(f)
+
+      val left = mp.run(rng)
       val right = RNG.map2(d1,d2)(f)(rng)
 
       left mustEqual right
@@ -94,9 +103,10 @@ object Ex10Spec extends Specification {
     "return the same results as RNG sequence" in {
       // List with 42 rand ints
       val l = List.fill(42)(RNG.int)
+      val ls = l.map(State(_))
 
       val left = RNG.sequence(l)(rng)
-      val right = State.sequence(l)(rng)
+      val right = State.sequence(ls).run(rng)
 
       left mustEqual right
     }
