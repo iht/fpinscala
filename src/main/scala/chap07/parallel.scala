e1e1d9a57823f445f3f5770cf5dd4ef701008e6b
Israel Herraiz <israel.herraiz@bbvadata.com>
2017-10-27 18:15:29 +0200

    Fix copyright year

--- src/main/scala/chap07/parallel.scala
+++ src/main/scala/chap07/parallel.scala
@@ -1,6 +1,6 @@
 package chap07
 
-/* Copyright (c) 2014-2016 Israel Herraiz <isra@herraiz.org>
+/* Copyright (c) 2014-2017 Israel Herraiz <isra@herraiz.org>
  *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
