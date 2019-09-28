3c511dd222b8838f7d11e6eb928ab1ac2aa3ae59
Israel Herraiz <ihr@google.com>
2019-09-28 20:21:19 +0200

    Update copyright

--- src/test/scala/chap06/ex10Spec.scala
+++ src/test/scala/chap06/ex10Spec.scala
@@ -1,5 +1,6 @@
-/* Copyright (c) 2014-2016 Israel Herraiz <isra@herraiz.org>
- * 
+/*
+ * Copyright (c) 2014-2019 Israel Herraiz <isra@herraiz.org>
+ *
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
